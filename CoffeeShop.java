7c836dccc98fcfbf1acf00d0e66e2ba75c985cad
Zack Considine <zack_considine@m015354.local>
2020-09-10 14:02:18 -0700

    pushing coffeeshop basics

---
+++ CoffeeShop.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class CoffeeShop {
+
+  public static void main(String[] args) {
+
+    System.out.println("");
+    System.out.println("Welcome to the Coffee Shop");
+    System.out.println("");
+
+    Scanner userIn = new Scanner(System.in);
+    String name = userIn.next();
+
+    System.out.println("Is your name, " + name);
+
+  }
+
+}
