e51e50af03783eb9a08a5922d745c44a8643881a
Zack Considine <zack_considine@m015354.aw.org>
2021-03-15 14:21:53 -0700

    added next button to go through questions randomly

--- Flashcards.java
+++ Flashcards.java
@@ -15,6 +15,7 @@ public class Flashcards {
   JButton next;
   JLabel question;
   JLabel correct;
+  ArrayList<String> questions;
 
   public Flashcards() {
 
@@ -23,13 +24,37 @@ public class Flashcards {
     flashWin.setVisible(true);
     flashWin.setLayout(null);
 
+    //System.out.println(x);
+
     response = new JTextArea("Type response here", 10, 20);
     flashWin.add(response);
     response.setBounds(200, 350, 200, 50);
 
     question = new JLabel("Sample Question");
     flashWin.add(question);
-    question.setBounds(250, 150, 200, 50);
+    question.setBounds(250, 150, 300, 50);
+
+    questions = new ArrayList<String>();
+
+    next = new JButton("Next Question");
+    flashWin.add(next);
+    next.setBounds(400,200,200,50);
+    next.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        Random r = new Random();
+        int sizeA = questions.size();
+        if (sizeA > 0) {
+          int choice = r.nextInt(questions.size());
+          String qu = questions.get(choice);
+          question.setText(qu);
+
+          questions.remove(choice);
+        }
+        else {
+          question.setText("no more questions :(");
+        }
+      }
+    });
 
     flashWin.addWindowListener(new WindowAdapter() {
        public void windowClosing(WindowEvent windowEvent){
@@ -38,36 +63,23 @@ public class Flashcards {
     });
   }
 
-  public void updateQuestion(String q) {
-    question.setText(q);
-  }
-
   public static void main(String[] args) {
-    Flashcards f = new Flashcards();
-    f.updateQuestion("Hey class. What is the weather?");
+    int x = 5;
 
     HashMap<String, String> cards = new HashMap<String, String>();
     cards.put("What is the capital of Washington?", "Olympia");
 
-    ArrayList<String> questions = new ArrayList<String>();
-    questions.add("What is the capital of Washington?");
-    questions.add("Who is the president of the U.S.?");
-    questions.add("What's 1 + 1?");
-    questions.add("How do you say tree in french?");
-    questions.add("What is the meaning of life?");
+    Flashcards f = new Flashcards();
+    f.questions.add("What is the capital of Washington?");
+    f.questions.add("Who is the president of the U.S.?");
+    f.questions.add("What's 1 + 1?");
+    f.questions.add("How do you say tree in french?");
+    f.questions.add("What is the meaning of life?");
 
-    System.out.println(questions.size());
+    System.out.println(f.questions.size());
 
-    Random r = new Random();
-    int sizeA = questions.size();
-    for (int i=0; i<sizeA; i++) {
-      int choice = r.nextInt(questions.size());
-      String qu = questions.get(choice);
-      System.out.println(qu);
-      System.out.println(cards.get(qu));
-      questions.remove(choice);
-    }
-    //System.out.println(r.nextInt(10));
+
+    f.question.setText("Ready for the questions? Hit next!");
 
   }
 
