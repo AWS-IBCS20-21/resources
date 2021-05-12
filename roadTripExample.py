db2062c13299e4e990a8afb479164be17e19b870
Zack Considine <zack_considine@m015354.aw.org>
2021-05-12 14:14:42 -0700

    added road trip example

---
+++ roadTripExample.py
@@ -0,0 +1,26 @@
+
+def listPotentialStops(destinationList, fTime, maxTime):
+
+    myStops = []
+    
+
+    return myStops
+
+def main():
+
+    print("Welcome to the Road trip")
+
+    pointA = "Tacoma"
+    pointB = "San Francisco"
+    fastestTime = 13.0
+    maxTime = 20.0
+
+    destinations = [("Children's Museum", .16 ), ("Mt. Shasta", 1.5), ("Powell's Bookstore", .5)]
+    print(destinations[0])
+    print("\n")
+    print(destinations[0][0])
+    print(destinations[2][1])
+
+
+if __name__ == "__main__":
+    main()
